6a6fd8637e4b0ff905a56d1cbe4425ef639647ea
leoleguizamon97 <anlleguizamongu@unal.edu.co>
2023-09-24 12:54:58 -0500

    fixed center title

--- Front/src/App.vue
+++ Front/src/App.vue
@@ -1,37 +1,32 @@
 <template>
-
 	<div id="Contenido" class="">
 		<NavComp />
 		<router-view v-slot="{ Component }">
-				<component :is="Component" :key="$route.path"></component>
+			<component :is="Component" :key="$route.path"></component>
 		</router-view>
 	</div>
 
 	<div id="footer" class="navbar fixed-bottom bg-body-secondary p-0">
-		<div id="f-container" class="container-fluid ">
-			<div id="f-about" class=" d-flex nav-item justify-content-around nav-item w-75">
-				<router-link class="router-link nav-link" to="/about">FAQ</router-link>
-				<router-link class="router-link nav-link d-none d-md-block" to="/about">Quienes somos</router-link>
-				<router-link class="router-link nav-link d-none d-md-block" to="/about">Terminos y condiciones</router-link>
-				<router-link class="router-link nav-link d-none d-md-block" to="/about">Comunidad</router-link>
-			</div>
-			<div id="f-redes" class="d-flex nav-item justify-content-end w-25">
-				<a class="rounded nav-link d-inline-block mx-1"
-					href="https://www.facebook.com/profile.php?id=61550256754354">
-					<i class="bi bi-facebook"></i>
-				</a>
-				<a class="rounded nav-link d-inline-block mx-1" href="https://twitter.com/iTalento_">
-					<i class="bi bi-twitter"></i>
-				</a>
-				<a class="rounded nav-link d-inline-block mx-1" href="https://www.instagram.com/italento_/">
-					<i class="bi bi-instagram"></i>
-				</a>
-				<a class="rounded nav-link d-inline-block mx-1" href="https://github.com/Kanto-Software-Solutions">
-					<i class="bi bi-github"></i>
-				</a>
-			</div>
+		<div id="f-about" class="d-flex nav-item justify-content-around w-75">
+			<router-link class="router-link nav-link" to="/about">FAQ</router-link>
+			<router-link class="router-link nav-link d-none d-md-block" to="/about">Quienes somos</router-link>
+			<router-link class="router-link nav-link d-none d-md-block" to="/about">Terminos y condiciones</router-link>
+			<router-link class="router-link nav-link d-none d-md-block" to="/about">Comunidad</router-link>
 		</div>
-
+		<div id="f-redes" class="d-flex nav-item justify-content-end w-25">
+			<a class="rounded nav-link d-inline-block mx-1" href="https://www.facebook.com/profile.php?id=61550256754354">
+				<i class="bi bi-facebook"></i>
+			</a>
+			<a class="rounded nav-link d-inline-block mx-1" href="https://twitter.com/iTalento_">
+				<i class="bi bi-twitter"></i>
+			</a>
+			<a class="rounded nav-link d-inline-block mx-1" href="https://www.instagram.com/italento_/">
+				<i class="bi bi-instagram"></i>
+			</a>
+			<a class="rounded nav-link d-inline-block mx-1" href="https://github.com/Kanto-Software-Solutions">
+				<i class="bi bi-github"></i>
+			</a>
+		</div>
 	</div>
 </template>
 
--- Front/src/components/NavComp.vue
+++ Front/src/components/NavComp.vue
@@ -1,16 +1,78 @@
 <template>
 	<nav class="navbar navbar-expand-lg bg-body-secondary sticky-top">
-		<div class="">
-
-			<div class="offcanvas offcanvas-start" id="elementosOcultos">
-				<div class="offcanvas-header">
-					<h5 class="offcanvas-title" id="offcanvasNavbarLabel">I-Talento</h5>
-					<button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-				</div>
-				<div class="offcanvas-body">
-					<div class="navbar-nav flex-grow-1">
-						<li class="dropdown">
+		<div class="offcanvas offcanvas-start" id="Menu">
+			<div class="offcanvas-header">
+				<h5 class="offcanvas-title" id="offcanvasNavbarLabel">I-Talento</h5>
+				<button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+			</div>
+			<div class="offcanvas-body">
+				<div class="navbar-nav">
+					<li id="CategoriasDropdown" class="dropdown">
 						<a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" role="button"
+							data-bs-toggle="dropdown" aria-expanded="false">
+							Categorías
+						</a>
+						<ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+							<li><a class="dropdown-item" href="#">Diseño gráfico</a></li>
+							<li><a class="dropdown-item" href="#">Marketing digital</a></li>
+							<li><a class="dropdown-item" href="#">Escritura y traducción</a></li>
+							<li><a class="dropdown-item" href="#">Vídeo y animación</a></li>
+							<li><a class="dropdown-item" href="#">Música y audio</a></li>
+							<li><a class="dropdown-item" href="#">Programación y tecnología</a></li>
+						</ul>
+					</li>
+				</div>
+			</div>
+		</div>
+
+		<div class="container-fluid justify-content-between">
+			<div id="Comodin" class="d-none d-lg-block"></div>
+			<button id="BotonToggler" class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#Menu"
+				aria-controls="elementosOcultos" aria-expanded="false">
+				<img src="../assets/img/logo.png" height="30" img />
+			</button>
+			<div id="Titulo">
+				<a class="nav-link navbar-brand" href="/">
+					<strong>
+						<large>i - Talento</large>
+					</strong>
+				</a>
+			</div>
+			<div id="Funciones">
+				<button class="btn " type="button" data-bs-toggle="collapse" data-bs-target="#buscar" aria-expanded="false"
+					aria-controls="buscar">
+					<i class="bi bi-search"></i>
+				</button>
+				<button class="btn" type="button" data-bs-toggle="collapse" data-bs-target="#buscar" aria-expanded="false"
+					aria-controls="buscar ">
+					<i class="bi bi-person"></i>
+				</button>
+			</div>
+		</div>
+	</nav>
+	<div class="collapse container text-center" id="buscar">
+		<div class="row justify-content-lg-center">
+				<form class="col-lg-auto d-flex" role="search">
+					<input class="form-control m-1" type="search" placeholder="Search" aria-label="Search">
+					<button class="btn m-1 btn-outline-success" type="submit">
+						<i class="bi bi-search"></i>
+					</button>
+				</form>
+
+
+		</div>
+	</div>
+
+	<nav class="navbar navbar-expand-lg navbar-light bg-light navbar-custom-center">
+		<div class="container-fluid navbar-container">
+			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+				aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarNavDropdown">
+				<ul class="navbar-nav">
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
 							data-bs-toggle="dropdown" aria-expanded="false">
 							Categorías
 						</a>
@@ -24,40 +86,33 @@
 
 						</ul>
 					</li>
-					</div>
-				</div>
-			</div>
-			<div>
-				<button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#elementosOcultos"
-					aria-controls="elementosOcultos" aria-expanded="false" aria-label="Toggle navigation">
-					<img src="../assets/img/logo.png" height="35" img/>
-				</button>
-
-			</div>
-			<div>
-				<a class="nav-link navbar-brand" href="/">
-					<strong>
-						<large>i - Talento</large>
-					</strong>
-				</a>
-			</div>
-			<div>
-				<button class="btn" type="button" data-bs-toggle="collapse" data-bs-target="#buscar" aria-expanded="false"
-					aria-controls="buscar">
-					<i class="bi bi-search"></i>
-				</button>				
-			</div>
-
+					<li class="nav-item">
+						<a class="nav-link" href="#">Cómo funciona</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link txtEncuentra" href="#">Encuentra freelancers</a>
+					</li>
+					<li>
+						<!--<a class="navbar-brand" href="#">
+						<img src="../assets/img/negro.jpg" alt="" width="70" height="37" class="d-inline-block align-text-top">
+						</a>-->
+						<a class="nav-link txtiTalento" href="/"><strong>
+								<large>iTALENTO</large>
+							</strong></a>
+					</li>
+					<li class="nav-item">
+						<button class="nav-link btn btn-link btn-login"
+							onclick="window.location.href='/login'">Login</button>
+					</li>
+					<li class="nav-item">
+						<button class="nav-link btn btn-link btn-signIn" onclick="window.location.href='/registro'">Sign
+							in</button>
+					</li>
+					<li class="nav-item">
+						<button class="nav-link btn btn-link btn-muestra" onclick="window.location.href='/registro'">Muestra
+							tu talento</button>
+				</li>
+			</ul>
 		</div>
-	</nav>
-
-	<div class="collapse" id="buscar">
-		<form class="d-flex container-sm" role="search">
-			<input class="form-control m-1" type="search" placeholder="Search" aria-label="Search">
-			<button class="btn m-1 btn-outline-success" type="submit">
-				<i class="bi bi-search"></i>
-			</button>
-		</form>
 	</div>
-
-</template>
+</nav></template>
