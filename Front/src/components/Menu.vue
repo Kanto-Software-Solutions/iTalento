627d2d1f8efdb5292dc83219d7e640ca0233f4f6
leoleguizamon97 <anlleguizamongu@unal.edu.co>
2023-09-24 14:56:21 -0500

    fix center title navbar

---
+++ Front/src/components/Menu.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="navbar-nav">
+		<li id="CategoriasDropdown" class="dropdown">
+			<a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown"
+				aria-expanded="false">
+				Categorías
+			</a>
+			<ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+				<li><a class="dropdown-item" href="#">Diseño gráfico</a></li>
+				<li><a class="dropdown-item" href="#">Marketing digital</a></li>
+				<li><a class="dropdown-item" href="#">Escritura y traducción</a></li>
+				<li><a class="dropdown-item" href="#">Vídeo y animación</a></li>
+				<li><a class="dropdown-item" href="#">Música y audio</a></li>
+				<li><a class="dropdown-item" href="#">Programación y tecnología</a></li>
+			</ul>
+		</li>
+		<a class="nav-link" href="#">Cómo funciona</a>
+		<a class="nav-link" href="#">Encuentra freelancers</a>
+	</div>
+</template>
--- Front/src/components/NavComp.vue
+++ Front/src/components/NavComp.vue
@@ -1,44 +1,31 @@
 <template>
-	<nav class="navbar navbar-expand-lg bg-body-secondary sticky-top">
-		<div class="offcanvas offcanvas-start" id="Menu">
-			<div class="offcanvas-header">
-				<h5 class="offcanvas-title" id="offcanvasNavbarLabel">I-Talento</h5>
-				<button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-			</div>
-			<div class="offcanvas-body">
-				<div class="navbar-nav">
-					<li id="CategoriasDropdown" class="dropdown">
-						<a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" role="button"
-							data-bs-toggle="dropdown" aria-expanded="false">
-							Categorías
-						</a>
-						<ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-							<li><a class="dropdown-item" href="#">Diseño gráfico</a></li>
-							<li><a class="dropdown-item" href="#">Marketing digital</a></li>
-							<li><a class="dropdown-item" href="#">Escritura y traducción</a></li>
-							<li><a class="dropdown-item" href="#">Vídeo y animación</a></li>
-							<li><a class="dropdown-item" href="#">Música y audio</a></li>
-							<li><a class="dropdown-item" href="#">Programación y tecnología</a></li>
-						</ul>
-					</li>
-				</div>
-			</div>
+	<div id="Menu" class="offcanvas offcanvas-start">
+		<div class="offcanvas-header">
+			<h5 class="offcanvas-title" id="offcanvasNavbarLabel">I-Talento</h5>
+			<button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
 		</div>
+		<div class="offcanvas-body">
+			<Menu />
+		</div>
+	</div>
 
-		<div class="container-fluid justify-content-between">
-			<div id="Comodin" class="d-none d-lg-block"></div>
-			<button id="BotonToggler" class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#Menu"
-				aria-controls="elementosOcultos" aria-expanded="false">
+	<nav class="navbar navbar-expand-lg bg-body-secondary sticky-top p-0">
+		<div class="container-fluid">
+			<button id="BotonToggler" class="navbar-toggler p-0 border-0" type="button" data-bs-toggle="offcanvas"
+				data-bs-target="#Menu" aria-controls="elementosOcultos" aria-expanded="false">
 				<img src="../assets/img/logo.png" height="30" img />
 			</button>
-			<div id="Titulo">
-				<a class="nav-link navbar-brand" href="/">
+			<div id="Menus" class="d-none d-lg-block">
+				<Menu />
+			</div>
+			<div id="Titulo" class="">
+				<a class="nav-link" href="/">
 					<strong>
 						<large>i - Talento</large>
 					</strong>
 				</a>
 			</div>
-			<div id="Funciones">
+			<div id="Funciones" class="">
 				<button class="btn " type="button" data-bs-toggle="collapse" data-bs-target="#buscar" aria-expanded="false"
 					aria-controls="buscar">
 					<i class="bi bi-search"></i>
@@ -47,72 +34,30 @@
 					aria-controls="buscar ">
 					<i class="bi bi-person"></i>
 				</button>
-			</div>
+			</div>		
 		</div>
+
+
+
 	</nav>
-	<div class="collapse container text-center" id="buscar">
+	<div id="buscar" class="collapse container">
 		<div class="row justify-content-lg-center">
-				<form class="col-lg-auto d-flex" role="search">
-					<input class="form-control m-1" type="search" placeholder="Search" aria-label="Search">
-					<button class="btn m-1 btn-outline-success" type="submit">
-						<i class="bi bi-search"></i>
-					</button>
-				</form>
-
-
+			<form class="col-lg-auto d-flex w-100" role="search">
+				<input class="form-control m-1" type="search" placeholder="Search" aria-label="Search">
+				<button class="btn m-1 btn-outline-secondary" type="submit">
+					<i class="bi bi-search"></i>
+				</button>
+			</form>
 		</div>
 	</div>
+</template>
 
-	<nav class="navbar navbar-expand-lg navbar-light bg-light navbar-custom-center">
-		<div class="container-fluid navbar-container">
-			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
-				aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-				<span class="navbar-toggler-icon"></span>
-			</button>
-			<div class="collapse navbar-collapse" id="navbarNavDropdown">
-				<ul class="navbar-nav">
-					<li class="nav-item dropdown">
-						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
-							data-bs-toggle="dropdown" aria-expanded="false">
-							Categorías
-						</a>
-						<ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-							<li><a class="dropdown-item" href="#">Diseño gráfico</a></li>
-							<li><a class="dropdown-item" href="#">Marketing digital</a></li>
-							<li><a class="dropdown-item" href="#">Escritura y traducción</a></li>
-							<li><a class="dropdown-item" href="#">Vídeo y animación</a></li>
-							<li><a class="dropdown-item" href="#">Música y audio</a></li>
-							<li><a class="dropdown-item" href="#">Programación y tecnología</a></li>
-
-						</ul>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="#">Cómo funciona</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link txtEncuentra" href="#">Encuentra freelancers</a>
-					</li>
-					<li>
-						<!--<a class="navbar-brand" href="#">
-						<img src="../assets/img/negro.jpg" alt="" width="70" height="37" class="d-inline-block align-text-top">
-						</a>-->
-						<a class="nav-link txtiTalento" href="/"><strong>
-								<large>iTALENTO</large>
-							</strong></a>
-					</li>
-					<li class="nav-item">
-						<button class="nav-link btn btn-link btn-login"
-							onclick="window.location.href='/login'">Login</button>
-					</li>
-					<li class="nav-item">
-						<button class="nav-link btn btn-link btn-signIn" onclick="window.location.href='/registro'">Sign
-							in</button>
-					</li>
-					<li class="nav-item">
-						<button class="nav-link btn btn-link btn-muestra" onclick="window.location.href='/registro'">Muestra
-							tu talento</button>
-				</li>
-			</ul>
-		</div>
-	</div>
-</nav></template>
+<script>
+import Menu from '@/components/Menu.vue'
+export default {
+	name: 'NavComp',
+	components: {
+		Menu
+	},
+}
+</script>
