2b8c97eb89abc4dbfab84fd75e628ed104d41b18
leoleguizamon97 <anlleguizamongu@unal.edu.co>
2023-09-28 05:48:50 -0500

    DIANA FRONT

--- Front/src/main.js
+++ Front/src/main.js
@@ -7,5 +7,4 @@ import '@/assets/bootstrap.bundle.min.js'
 import '@/assets/custom_styles.css'
 import 'font-awesome/css/font-awesome.min.css'
 import 'material-icons/iconfont/material-icons.css'
-
 createApp(App).use(store).use(router).mount('#app')
--- Front/vue.config.js
+++ Front/vue.config.js
@@ -1,5 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  outputDir: '../Back/src/public', // Nombre de la carpeta de salida para archivos estáticos
-});
+  transpileDependencies: true
+})
