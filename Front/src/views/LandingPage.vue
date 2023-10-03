7f2eeedb26a7c07a95f6d5524ba880a3b0aa5e11
leoleguizamon97 <anlleguizamongu@unal.edu.co>
2023-10-03 11:18:55 -0500

    terminado landingMain

--- Front/src/views/LandingPage.vue
+++ Front/src/views/LandingPage.vue
@@ -1,22 +1,27 @@
 <template>
-	<div id="LandingMain" class="position-relative" >
+	<div id="LandingMain" class="position-relative">
 		<div class="overflow-hidden">
-			<img src="../assets/img/FondoLanding.png" class="position-absolute z-n1 w-100 h-100 object-fit-cover" alt="fondo" style="opacity:0.8;" >
+			<img src="../assets/img/FondoLanding.png" class="position-absolute z-n1 w-100 h-100 object-fit-cover" alt="fondo">
 		</div>
 		<div class="row m-1">
 			<div class="col-sm col h-100 m-1 align-self-center">
-					<p class="text-start fs-3" style="color: rgb(255,255,255,0.9)">
+					<p class="text-start fs-1 fw-bold" style="color: rgb(199, 199, 199)">
 						No te preocupes, en i-Talento
 						tenemos lo que buscas para tus proyectos personales o tu empresa.
+					</p>
+					<p class="text-start fs-1 fw-bold" style="color: rgb(199, 199, 199)">
 						Da un paso adelante y busca eso que necesitas.
+					</p>
+					<p>
+
 					</p>
 			</div>
 			<div class="col-sm h-100 align-self-end">
-				<div class="row m-1 d-none d-sm-block" style="height: 50vh">
+				<div class="row m-1 d-none d-sm-block" style="min-height: 75vh">
 					<!--Comodin de altura-->
 				</div>
-				<div class="row mx-auto my-2 w-75">
-					<button class="btn btn-secondary" type="button">
+				<div class="row mx-auto my-5" style="width: 60%;">
+					<button class="btn btn-outline-dark bg-body-secondary bg-opacity-50" type="button">
 						Busca Talento
 					</button>
 				</div>
