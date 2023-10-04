09801dc8659bb52585d042b4c51e4fb081b40f6a
leoleguizamon97 <anlleguizamongu@unal.edu.co>
2023-10-04 10:01:30 -0500

    aboout us inicial

--- Front/src/views/AboutUs.vue
+++ Front/src/views/AboutUs.vue
@@ -1,25 +1,19 @@
 <template>
-	<div id="LandingMain" class="position-relative">
+	<div id="AboutMain" class="position-relative">
 		<div class="overflow-hidden">
-			<img src="../assets/img/FondoLanding.png" class="position-absolute z-n1 w-100 h-100 object-fit-cover"
-				alt="fondo">
+			<img src="https://img.freepik.com/foto-gratis/silueta-ejecutivos-comerciales-seguros_1098-1768.jpg?w=1380&t=st=1696395737~exp=1696396337~hmac=c77339dc338757c4428e44a88c1a9328f7db4b1fdf66a89f2670623adcdb2e9d"
+				class="position-absolute z-n1 w-100 h-100 object-fit-cover" alt="fondo" style="object-position: top;">
 		</div>
 		<div class="container-xl">
 			<div class="row">
 				<div class="col-sm col m-1 align-self-center d-flex justify-content-center">
-					<div class="">
-						<p class="text-justify fs-1 fw-bold" style="color: rgb(199, 199, 199)">
-							No te preocupes, en i-Talento
-							tenemos lo que buscas para tus proyectos personales o tu empresa.
+					<div class=" bg-black rounded-4 p-3 bg-opacity-50">
+						<p class="text-justify fs-1 fw-bold text-white">
+							Sobre nuestro equipo:
 						</p>
-						<p class="text-Justify fs-1 fw-bold" style="color: rgb(199, 199, 199)">
-							Da un paso adelante y busca eso que necesitas.
+						<p class="text-Justify fs-4 text-white">
+							Quienes somos y por que lo hacemos.
 						</p>
-						<ul class="text-body">
-							<li style="color: rgb(199, 199, 199)">Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
-							<li style="color: rgb(199, 199, 199)">Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
-							<li style="color: rgb(199, 199, 199)">Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
-						</ul>
 					</div>
 				</div>
 				<div class="col-sm h-100 align-self-end">
@@ -27,11 +21,38 @@
 						<!--Comodin de altura-->
 					</div>
 					<div class="row mx-auto my-5" style="width: 60%;">
-						<button class="btn" type="button"
-							style="background-color: rgba(58, 58, 58, 0.4); color: rgb(199, 199, 199);">
-							Busca Talento
+						<button class="btn router-link btn-primary" type="button" to="/">¡Apoyanos en PayPal!
+							<i class="bi bi-paypal"></i>
 						</button>
 					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div id="AboutContenido" class="container-xl p-0">
+		<div class="card border-0 m-2">
+			<div class="card-body row align-items-center">
+				<div class="col-sm">
+					<h3 class="card-header text-center bg-transparent">Kanto Software Solutions</h3>
+					<p class="text-center">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit officiis, veritatis quia
+						quis dolorum debitis dolorem quod laudantium eum aspernatur soluta consequuntur dicta quae assumenda
+						neque iste doloremque ullam velit?
+					</p>
+				</div>
+				<div class="col-sm">
+					<img src="https://wallpapercave.com/dwp1x/wp1892403.jpg" class="w-100 h-100 object-fit-cover rounded-5"
+						alt="fondo">
+				</div>
+			</div>
+		</div>
+	</div>
+	<div id="AboutNosotros" class="container-xl">
+		<div class="card border-0 m-2">
+			<div class="card-body row align-items-center">
+				<div class="col-sm">
+					<img src="https://wallpapercave.com/dwp1x/wp1892403.jpg" class="w-100 h-100 object-fit-cover rounded-5"
+						alt="fondo">
 				</div>
 			</div>
 		</div>
@@ -39,6 +60,4 @@
 </template>
 <script>
 </script>
-<style>
-	
-</style>
+<style></style>
