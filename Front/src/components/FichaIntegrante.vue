8c74b2041e2e92a85cf69ad724d23452c2adbf78
leoleguizamon97 <anlleguizamongu@unal.edu.co>
2023-10-04 12:39:01 -0500

    ficha integrante creada

---
+++ Front/src/components/FichaIntegrante.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="d-inline" style="width:300px;">
+		<img src="" alt="Foto">
+		<img src="" alt="pokemon">
+		<div class="w-75">
+			<h5 class="card-title">{{ nombre }}</h5>
+			<p class="card-text">{{ descripcion }}</p>
+		</div>
+	</div>
+</template>
+<script>
+export default {
+	props: {
+		nombre: String,
+		descripcion: String,
+		imagen: String,
+		pokemon: String,
+	},
+}
+</script>
