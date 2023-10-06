c27be8a43dd497cbd508b956a11084ae793b856f
nirojasm-u <nirojasm@unal.edu.co>
2023-10-06 17:26:29 -0500

    Sequelize para hacer la API, UserModel.

---
+++ Back/src/models/User.js
@@ -0,0 +1,66 @@
+const { DataTypes } = require("sequelize");
+
+const UserModel = {
+  idUser: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  names: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  lastNames: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  isVerified: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    default: false,
+  },
+  nickname: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  profileImage: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  isFreelancer: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    default: false,
+  },
+  birthDate: {
+    type: DataTypes.DATE,
+    allowNull: true,
+  },
+  country: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  acceptedTerms: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    default: false,
+  },
+  personaId: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  }
+};
+
+module.exports = {
+    initialize: (sequelize) => {
+      this.model = sequelize.define("user", UserModel);
+    },
+  
+    createUser: (user) => {
+      return this.model.create(user);
+    }
+};
