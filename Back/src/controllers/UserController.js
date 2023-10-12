f9a3fe6166dfd1d517900ba74712f2c5d596d85d
nirojasm-u <nirojasm@unal.edu.co>
2023-10-12 11:15:33 -0500

    GetAllUsers

--- Back/src/controllers/UserController.js
+++ Back/src/controllers/UserController.js
@@ -1,1 +1,13 @@
 const conexion	= require('../database/db');
+
+const GetAllUsers = (req,res) => {
+	conexion.query("SELECT * FROM User", (error,results) => {
+		if(error){
+			console.log(error);
+		}else{
+			res.json({results:results});
+		}
+	});
+};
+
+module.exports.GetAllUsers = GetAllUsers;
--- Back/src/controllers/router.js
+++ Back/src/controllers/router.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const crud = require('./crud');
-const usuario = require('./UserController')
+const usuario = require('./UserController');
+const gig = require('./GigController');
 //Permite ser usado en otros archivos
 module.exports = router;
 
 //Segun el metodo de la peticion, se ejecuta la funcion correspondiente
-router.get('/usuarios', 		crud.usuarios);
+router.get('/usuarios', 		usuario.GetAllUsers);
 router.get('/publicaciones',	crud.publicaciones);
 router.get('/test',				crud.test);
