d2b28810406d7bf3a200f359c4f80995fd97ac1c
GurenSeiken <kevinsnivia@hotmail.com>
2023-10-17 08:11:01 -0500

    separación de cada controlador de la base de datos

--- Back/src/controllers/LanguageController.js
+++ Back/src/controllers/LanguageController.js
@@ -1,1 +1,30 @@
 const conexion	= require('../database/db');
+
+
+exports.createLanguage = (req,res) =>{
+	const idLanguage = req.body.idLanguaje;
+	const name = req.body.name;
+
+	conexion.query("INSERT INTO Language (idLanguage, name) VALUES ( '" + idLanguage + "' ,'" + name + ")", (error, results) => {
+		if(error){
+			console.log(error);
+		}else{
+			console.log("Se agregó el idioma");
+		}
+	});
+}
+
+exports.updateLanguaje = (req,res) =>{
+	const idLanguaje = req.body.idLanguaje;
+	const name = req.body.name;
+
+	let query = ('update Languaje set idLanguaje =' + idLanguaje + ' , name="'+name);
+	conexion.query(query,(error,results)=>{
+		if(error){
+			console.log(error);
+		}else{
+			console.log("Se actualizo el Lenguaje: ");
+			res.redirect('/home');
+		}
+	});
+}
--- Back/src/controllers/OrderController.js
+++ Back/src/controllers/OrderController.js
@@ -1,1 +1,36 @@
 const conexion	= require('../database/db');
+
+exports.createOrder = (req,res) =>{
+	const idOrder = req.body.idOrder;
+	const createdAt = req.body.createdAt;
+	const status = req.body.status;
+	const idUser = req.body.idUser;
+	const idGig = req.body.idGig;
+
+	conexion.query("INSERT INTO Order (idOrder, createdAt, status,idUser,idGig) VALUES ( '" + idOrder + "' ,'" + createdAt + "' ,'" + status + "' ,'" + idUser + "' ,'" + idGig + ")", (error, results) => {
+		if(error){
+			console.log(error);
+		}else{
+			console.log("Se agregó la orden ");
+			res.redirect('/home');
+		}
+	});
+}
+
+exports.updateOrder = (req,res) =>{
+	const idOrder = req.body.idOrder;
+	const createdAt = req.body.createdAt;
+	const status = req.body.status;
+	const idUser = req.body.idUser;
+	const idGig = req.body.idGig;
+
+	let query = ('update Order set idOrder =' + idOrder + ' , createdAt="'+createdAt+'",status="'+status+ '", idUser = '+ idUser + '", idGig = '+ idGig );
+	conexion.query(query,(error,results)=>{
+		if(error){
+			console.log(error);
+		}else{
+			console.log("Se actualizo la orden: ");
+			res.redirect('/home');
+		}
+	});
+}
