c6647b850711a5cc57c27a8e296d9778a488e0b2
GurenSeiken <kevinsnivia@hotmail.com>
2023-11-02 07:47:40 -0500

    Crud update

--- Back/src/controllers/SNetwork.js
+++ Back/src/controllers/SNetwork.js
@@ -17,3 +17,23 @@ exports.createRed = (req,res) =>{
 	});
 
 }
+
+
+exports.updateRed = (req,res) =>{
+
+    const idRed = body.req.idRed;
+    const idUser = body.req.idUser;
+    const nombre = body.req.nombre;
+    const link = body.req.link;
+
+    conexion.query('update SNetwork set SNetwork idRed =' + idRed + ' , idUser="'+ idUser+'", nombre="'+nombre );
+	conexion.query(query,(error,results)=>{
+		if(error){
+			console.log(error);
+		}else{
+			console.log("Se agregó la Red ");
+			
+		}
+	});
+
+}
