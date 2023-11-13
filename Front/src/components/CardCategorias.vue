ceaede609c3708a65d9cdb7766f110c83b0fa9af
leoleguizamon97 <anlleguizamongu@unal.edu.co>
2023-11-13 17:14:38 -0500

    tarjetas mejoradas

--- Front/src/components/CardCategorias.vue
+++ Front/src/components/CardCategorias.vue
@@ -1,13 +1,13 @@
 <template>
 	<div :id=id class="card text-bg-secondary overflow-hidden" style="height:500px ; width: 250px;">
 		<img :src=imagen class="w-100 h-100 object-fit-cover" alt="testPic">
-		<div class="card-img-overlay bg-black bg-opacity-25 p-0">
+		<div class="card-img-overlay bg-black bg-opacity-50 p-0">
 			<div class="h-25 p-3">
 				<h5>{{ id }} : {{ titulo }}</h5>
 			</div>
 			<div class="d-flex align-items-end w-100 h-75">
-				<div class="d-flex align-items-center w-100 bg-black opacity-50 h-25 p-1">
-					<p class="card-text">{{ descripcion }}</p>
+				<div class="d-flex align-items-center w-100 bg-black bg-opacity-50 h-25 p-2">
+					<p class="card-text text-center  w-100 ">{{ descripcion }}</p>
 				</div>
 			</div>
 		</div>
